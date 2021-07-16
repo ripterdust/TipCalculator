cbea317eb0fe9283b22f9db7808d3084d5c8373a
Bryan <bryantello2010@hotmail.com>
2021-07-15 23:02:54 -0600

    Agregado el texto

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { DollarSvg } from './DollarSvg'
+
 
 export const Result = () => {
     return <>
-        Resultados
+        <div>
+            <div className="info">
+                <div>
+                    <div className="text">
+                        <span>Tip</span>
+                        <span>/ person</span>
+                    </div>
+                    <div className="numbers">
+                        <DollarSvg />
+                        <span>0.00</span>
+                    </div>
+                </div>
+            </div>
+            <div className="info">
+                <div>
+                    <div className="text">
+                        <span>Total</span>
+                        <span>/ person</span>
+                    </div>
+                    <div className="numbers">
+                        <DollarSvg />
+                        <span>0.00</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button>Reset</button>
+
     </>
 }
