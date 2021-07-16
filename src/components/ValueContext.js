fe21681bbb2778694b232b800d6d6aa3ac3c76a3
Bryan <bryantello2010@hotmail.com>
2021-07-15 23:43:39 -0600

    Agregado el context

---
+++ src/components/ValueContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+
+export const ValueContext = createContext(null);
+
+export const defaultValues = {
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { ValueContext, defaultValues } from './components/ValueContext';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ValueContext.Provider>
+        <App />
+      </ValueContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
