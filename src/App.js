84e8a3a6ce2ec5aef035cb58b11b1c2e2076059b
Bryan <bryantello2010@hotmail.com>
2021-07-15 19:48:31 -0600

    Added title and classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Calculator } from './components/Calculator'
 import './styles/main.scss'
 
 
 export const App = () => {
     return (
-        <div>
-            Calculadora.
+        <div className="main_container">
+            <span>SPLI<br/>TTER</span>
+            <Calculator />
         </div>
     )
 }
