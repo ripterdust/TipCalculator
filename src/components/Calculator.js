d99d771e06ce1a22311e9a8866a79d3abda8b61b
Bryan <bryantello2010@hotmail.com>
2021-07-15 20:11:43 -0600

    Terminada la estructura inicial de las opciones

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,10 +3,32 @@ import React from 'react'
 export const Calculator = () => {
     return <div className="calculator">
         <div className="options">
-            Acá irán los formularios. :)
+            <div className="inputs bill">
+                <span>
+                    Bill
+                </span>
+                <input type="text" name="bill" />
+            </div>
+            <div className="tip">
+                <span>
+                    Select Tip %
+                </span>
+                <div className="buttons">
+                    <div className="btn">5%</div>
+                    <div className="btn">10%</div>
+                    <div className="btn">15%</div>
+                    <div className="btn">25%</div>
+                    <div className="btn">50%</div>
+                    <input type="text" placeholder='Custom' />
+                </div>
+            </div>
+            <div className="people">
+                <span>Number of people</span>
+                <input type="text" />
+            </div>
         </div>
-        <div className="results">
-            Acá irá el resultado :)
+        <div className=" inputs results">
+            Acá irá el resultado
         </div>
     </div>
 }
