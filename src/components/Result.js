9716b1353402ffd1db4094bf51826752f54bdb50
Bryan <bryantello2010@hotmail.com>
2021-07-15 23:12:24 -0600

    Agregados estilos al botón y eliminado el div extra

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,27 +6,23 @@ export const Result = () => {
     return <>
         <div>
             <div className="info">
-                <div>
-                    <div className="text">
-                        <span>Tip</span>
-                        <span>/ person</span>
-                    </div>
-                    <div className="numbers">
-                        <DollarSvg />
-                        <span>0.00</span>
-                    </div>
+                <div className="text">
+                    <span>Tip</span>
+                    <span>/ person</span>
+                </div>
+                <div className="numbers">
+                    <DollarSvg />
+                    <span>0.00</span>
                 </div>
             </div>
             <div className="info">
-                <div>
-                    <div className="text">
-                        <span>Total</span>
-                        <span>/ person</span>
-                    </div>
-                    <div className="numbers">
-                        <DollarSvg />
-                        <span>0.00</span>
-                    </div>
+                <div className="text">
+                    <span>Total</span>
+                    <span>/ person</span>
+                </div>
+                <div className="numbers">
+                    <DollarSvg />
+                    <span>0.00</span>
                 </div>
             </div>
         </div>
